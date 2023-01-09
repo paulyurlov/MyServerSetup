02be0c9213297d42b20d9159a841c84287a406c9
Paul Yurlov <paulyurlov@ya.ru>
2023-01-09 10:54:34 +0300

    Minor Update

--- script.sh
+++ script.sh
@@ -12,24 +12,16 @@ sudo apt update
 sudo apt install fish
 chsh -s /usr/bin/fish
 
-# Install roboto fonts
-sudo apt install fonts-roboto
-
-# Install oh-my-posh and theme
-
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-sudo chmod +x /usr/local/bin/oh-my-posh
-mkdir ~/.poshthemes
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-chmod u+rw ~/.poshthemes/*.omp.*
-rm ~/.poshthemes/themes.zip
-cp my-posh-config.json ~/.config/fish/
-echo "oh-my-posh init fish --config ~/.config/fish/my-posh-config.json | source" >~/.config/fish/config.fish
-exec fish
-oh-my-posh font install RobotoMono
-sudo oh-my-posh font install RobotoMono
-sudo cp ~/.local/share/fonts/robotomono-nerd-font-mono/Roboto\ Mono\ Nerd\ Font\ Complete\ Mono.ttf /usr/share/fonts/
+# Install noip DUC
+sudo apt install gcc g++ make build-essential
+sudo -s
+cd /usr/local/src/
+wget http://www.noip.com/client/linux/noip-duc-linux.tar.gz
+tar xf noip-duc-linux.tar.gz
+cd noip-2.1.9-1/
+make install
+/usr/local/bin/noip2 -C
+/usr/local/bin/noip2
 
 # Reboot part
 read -n 1 -p "Do uou want to reboot to apply settings (y/n)? " answer
