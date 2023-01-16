eea9d7ed40bc99b3fc95d0a1f4fe30cc07368993
Paul Yurlov <palyurlov@gmail.com>
2023-01-16 13:11:42 +0300

    Minor Update

--- script.sh
+++ script.sh
@@ -12,23 +12,33 @@ sudo apt update
 sudo apt install fish
 chsh -s /usr/bin/fish
 
-# Install noip DUC
-sudo apt install gcc g++ make build-essential
-sudo -s
-cd /usr/local/src/
-wget http://www.noip.com/client/linux/noip-duc-linux.tar.gz
-tar xf noip-duc-linux.tar.gz
-cd noip-2.1.9-1/
-make install
-/usr/local/bin/noip2 -C
-/usr/local/bin/noip2
+# Install neofetch
+sudo apt install neofetch
+
+# Install docker
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install aaPanel
+wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh aapanel
+
+# Install dokku
+wget https://dokku.com/install/v0.29.4/bootstrap.sh
+sudo DOKKU_TAG=v0.29.4 bash bootstrap.sh
+# cat ~/.ssh/authorized_keys | dokku ssh-keys:add admin
+# dokku domains:set-global pauly.ru
 
 # Reboot part
 read -n 1 -p "Do uou want to reboot to apply settings (y/n)? " answer
 printf "\n\n"
 if [[ $answer == "y" ]]; then
-    echo "Rebooting in 3 seconds"
-    sudo reboot -f
+  echo "Rebooting in 3 seconds"
+  sudo reboot -f
 else
-    echo "No reboot"
+  echo "No reboot"
 fi
